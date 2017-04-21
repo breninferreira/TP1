7bead9bc53d6b40828faff025a31d461e8690893
Breno Rios <brenoriosfe@hotmail.com>
2017-04-21 17:16:36 -0300

    Questão 6

--- b16-lab/test-ball.cpp
+++ b16-lab/test-ball.cpp
@@ -3,15 +3,25 @@
  ** author: Andrea Vedaldi
  **/
 
+/**
+Linhas referentes a tarefa:11a16,20,s22,23,25 
+**/
 #include "ball.h"
+
+void cont(Simulation & t, double dt){
+	for (int i = 0 ; i < 100 ; ++i) {
+	    t.step(dt) ;
+	    t.display() ;
+  }
+}
 
 int main(int argc, char** argv)
 {
-  Ball ball ;
+  Ball ball (0.1,0.1);
   const double dt = 1.0/30 ;
-  for (int i = 0 ; i < 100 ; ++i) {
-    ball.step(dt) ;
-    ball.display() ;
-  }
+  ball.setCoordx(0.5);
+  ball.setCoordy(0.5);
+
+  cont(ball,dt);
   return 0 ;
 }
